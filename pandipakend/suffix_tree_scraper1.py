3640d1138897ba5c2aefb340876de917825af2c6
Simmo Saan <simmo.saan@gmail.com>
2025-08-21 20:14:41 +0300

    Add possibly broken versions of suffix tree scraper with more pruning

---
+++ pandipakend/suffix_tree_scraper1.py
@@ -0,0 +1,76 @@
+import json
+import logging
+from typing import override
+
+import suffix_tree
+
+from pandipakend.database import AbstractDatabase
+from pandipakend.real_database import RealDatabase
+from pandipakend.scraper import AbstractScraper
+
+from .database import QueryCountDatabase
+from .mock_database import MockDatabase
+
+
+logger = logging.getLogger(__name__)
+
+
+class SuffixTreeScraper(AbstractScraper):
+    barcode_tree: suffix_tree.Tree
+    term_set: set[str]
+
+    def __init__(self, database: AbstractDatabase):
+        super().__init__(database)
+        self.barcode_tree = suffix_tree.Tree()
+        self.term_set = set()
+
+    @override
+    def scrape(self, term: str = ""):
+        def scrape(term, depth=0, fwd=True):
+            logger.debug("%s %s: %d", depth * " ", term, len(self.barcode_tree.find_all(term)))
+            for i in range(len(term)): # TODO: can check better with suffix_tree (matching statistics?)
+                # for j in range(i, len(term)):
+                j = len(term) # no point in checking others, they would've been rejected at upper scrape depths
+                if term[i:j + 1] in self.term_set:
+                    logger.debug("skipping %s %s", term, term[i:j + 1])
+                    return
+                # no point in checking others, they would've been rejected at upper scrape depths
+                if term[:i] in self.term_set: # for fwd=False
+                    logger.debug("skipping %s %s", term, term[:i])
+                    return
+            # need fwd=False, because if term is at end, then extending fwd will not find
+            if len(self.barcode_tree.find_all(term)) < 10:
+                result = self.database.query(term)
+                for package in result:
+                    if package["barcode"] not in self.packages:
+                        self.barcode_tree.add(package["barcode"], package["barcode"])
+                        self.packages[package["barcode"]] = package
+                        print(json.dumps(package, ensure_ascii=False))
+                # logger.info("%s %s: lookup", depth * " ", term)
+                if len(result) >= 10:
+                    for digit in range(0, 10):
+                        if fwd:
+                            scrape(term + str(digit), depth=depth+1, fwd=fwd)
+                        scrape(str(digit) + term, depth=depth+1, fwd=False)
+            else:
+                for digit in range(0, 10):
+                    if fwd:
+                        scrape(term + str(digit), depth=depth+1, fwd=fwd)
+                    scrape(str(digit) + term, depth=depth+1, fwd=False)
+            self.term_set.add(term) # should be conditional on fwd?
+
+        if term == "":
+            for digit in range(0, 10):
+                scrape(str(digit), depth=1)
+        else:
+            scrape(term)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    database = QueryCountDatabase(MockDatabase("11.txt"))
+    # database = QueryCountDatabase(RealDatabase())
+    scraper = SuffixTreeScraper(database)
+    scraper.scrape("")
+    logger.info("queries: %d", database.query_count)
+    logger.info("packages: %d", len(scraper.packages))
